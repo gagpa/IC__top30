5e2989d581e9493aa107083587a2a30449ed7dfe
ofry <speitshot@gmail.com>
2022-06-29 15:14:06 +0300

    Fix

--- app/api_v1/router.py
+++ app/api_v1/router.py
@@ -4,6 +4,7 @@ from .admin import router as admin_router
 from .auth import router as auth_router
 from .coach import router as coach_router
 from .registration import router as registration
+from .request import router as requests_router
 from .self import router as self_router
 from .student import router as student_router
 from .user import router as user_router
@@ -15,5 +16,6 @@ def add_routers(app: FastAPI):
     app.include_router(auth_router)
     app.include_router(student_router)
     app.include_router(self_router)
+    app.include_router(requests_router)
     app.include_router(registration)
     app.include_router(user_router)
