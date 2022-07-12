f7444447f9dc85ead8081761ed084c2e42a16b9f
ofry <speitshot@gmail.com>
2022-07-12 12:37:15 +0300

    Fix coach verifier

--- app/domain/student/resources/coach_verifier/postgres.py
+++ app/domain/student/resources/coach_verifier/postgres.py
@@ -15,8 +15,9 @@ class PostrgesCoachVerifier(CoachVerifier):
 
     async def is_free(self, coach_id: UUID):
         subquery_count_of_students = sql.select(sql.func.count(models.Student.id)).where(coach_id=coach_id).subquery()
+        subquery_coach = sql.select(models.Coach.id).join(models.User).where(models.User.uuid == coach_id).subquery()
         query = sql.select(models.Coach).where(
-            coach_id == coach_id,
+            models.Coach.coach_id == subquery_coach,
             models.Coach.total_seats > subquery_count_of_students,
         )
         cursor = await self.session.execute(query)
