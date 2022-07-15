03fa2e340ff410fd2fe7df23f41cda3acba3f858
ofry <speitshot@gmail.com>
2022-07-15 12:02:09 +0300

    Fix event

--- app/api_v1/router.py
+++ app/api_v1/router.py
@@ -9,12 +9,14 @@ from .self import router as self_router
 from .slot import router as slot_router
 from .student import router as student_router
 from .user import router as user_router
+from .event import router as event_router
 
 
 def add_routers(app: FastAPI):
     app.include_router(admin_router)
     app.include_router(coach_router)
     app.include_router(auth_router)
+    app.include_router(event_router)
     app.include_router(student_router)
     app.include_router(self_router)
     app.include_router(slot_router)
