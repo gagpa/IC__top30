9234e3303eeef3469befbd3db6d1e6bcc02e5e9c
ofry <speitshot@gmail.com>
2022-07-15 11:40:53 +0300

    Fix event

--- app/domain/event/resources/mover/base.py
+++ app/domain/event/resources/mover/base.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
-from uuid import UUID
 from datetime import datetime
+from uuid import UUID
+
 
 class EventMover(ABC):
 
     @abstractmethod
     async def move(self, event_id: UUID, new_start_date: datetime):
+        """Подвинуть событие"""
