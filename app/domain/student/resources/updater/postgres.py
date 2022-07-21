a7cf47a14c35806c97f6d77b43273fc16ecde41a
ofry <speitshot@gmail.com>
2022-07-21 16:45:21 +0300

    Log cancel event

--- app/domain/student/resources/updater/postgres.py
+++ app/domain/student/resources/updater/postgres.py
@@ -44,7 +44,10 @@ class PostgresStudentUpdater(StudentUpdater):
         if photo:
             user_subquery = select(models.User.id).where(models.User.uuid == student_id).subquery()
             delete_photo_query = delete(models.Photo).where(models.Photo.user_id == user_subquery)
-            await self.session.execute(delete_photo_query)
+            await self.session.execute(
+                delete_photo_query,
+                execution_options=immutabledict({"synchronize_session": 'fetch'}),
+            )
             update_photo_query = insert(models.Photo).values(img=photo, user_id=user_subquery)
             await self.session.execute(update_photo_query)
         if email:
