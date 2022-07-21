b708c375d2e7988da72c1146dee5704e08c360e8
ofry <speitshot@gmail.com>
2022-07-21 16:29:42 +0300

    Log cancel event

--- app/domain/student/resources/updater/postgres.py
+++ app/domain/student/resources/updater/postgres.py
@@ -42,6 +42,7 @@ class PostgresStudentUpdater(StudentUpdater):
         if phone:
             user_update_obj['phone'] = phone
         if photo:
+            print(photo)
             user_update_obj['photo'] = models.Photo(img=photo)
         if email:
             user_update_obj['email'] = str(email)
