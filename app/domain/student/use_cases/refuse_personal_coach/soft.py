d69fc7d824a9dadb8494eab6e2dbb33502afe85a
ofry <speitshot@gmail.com>
2022-07-21 14:08:10 +0300

    Fix Change refuse personal coach

--- app/domain/student/use_cases/refuse_personal_coach/soft.py
+++ app/domain/student/use_cases/refuse_personal_coach/soft.py
@@ -1,5 +1,7 @@
 from uuid import UUID
 
+from domain.event.resources.deleter import EventDeleter
+from domain.event.resources.repo import EventRepo
 from domain.student.resources.personal_coach_changer import PersonalCoachChanger
 from domain.student.resources.student_repo import StudentRepo
 from .base import RefusePersonalCoach
@@ -7,10 +9,21 @@ from .base import RefusePersonalCoach
 
 class SoftRefusePersonalCoach(RefusePersonalCoach):
 
-    def __init__(self, student_repo: StudentRepo, coach_changer: PersonalCoachChanger):
+    def __init__(
+            self,
+            student_repo: StudentRepo,
+            coach_changer: PersonalCoachChanger,
+            event_deleter: EventDeleter,
+            event_repo: EventRepo,
+    ):
         self.student_repo = student_repo
         self.coach_changer = coach_changer
+        self.event_deleter = event_deleter
+        self.event_repo = event_repo
 
     async def refuse(self, student_id: UUID):
         student = await self.student_repo.find(student_id)
+        events = await self.event_repo.filter(student_id=student_id, coach_id=None)
+        for event in events.items:
+            await self.event_deleter.delete(event.id)
         await self.coach_changer.change(student.user_id, new_coach=None)
