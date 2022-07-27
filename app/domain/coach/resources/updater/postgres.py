a571ac8a8655cea306cb3a84276735ea0da03647
ofry <speitshot@gmail.com>
2022-07-27 12:38:17 +0300

    Fix access

--- app/domain/coach/resources/updater/postgres.py
+++ app/domain/coach/resources/updater/postgres.py
@@ -44,15 +44,16 @@ class PostgresCoachUpdater(CoachUpdater):
             user_update_obj['email'] = str(email)
         if phone:
             user_update_obj['phone'] = phone
-        if photo:
+        if isinstance(photo, bytes):
             user_subquery = select(models.User.id).where(models.User.uuid == coach_id).subquery()
             delete_photo_query = delete(models.Photo).where(models.Photo.user_id == user_subquery)
             await self.session.execute(
                 delete_photo_query,
                 execution_options=immutabledict({"synchronize_session": 'fetch'}),
             )
-            update_photo_query = insert(models.Photo).values(img=photo, user_id=user_subquery)
-            await self.session.execute(update_photo_query)
+            if photo:
+                update_photo_query = insert(models.Photo).values(img=photo, user_id=user_subquery)
+                await self.session.execute(update_photo_query)
         if user_update_obj:
             user_update_query = update(models.User).where(models.User.uuid == coach_id)
             await self.session.execute(user_update_query.values(**user_update_obj))
