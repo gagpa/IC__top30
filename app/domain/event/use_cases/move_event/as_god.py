a2239dc6a146f428828f419303f1236ab4f1945a
ofry <speitshot@gmail.com>
2022-07-28 16:15:57 +0300

    Log god mode

--- app/domain/event/use_cases/move_event/as_god.py
+++ app/domain/event/use_cases/move_event/as_god.py
@@ -39,5 +39,5 @@ class MoveEventAsGod(MoveEvent):
                     start_date=start_date,
                     end_date=start_date + timedelta(hours=1),
                 )
-        await self.slot_repo.session.execute()
+        await self.slot_repo.session.commit()
         return await self.event_mover.move(event_id=event.id, new_start_date=new_start_date)
