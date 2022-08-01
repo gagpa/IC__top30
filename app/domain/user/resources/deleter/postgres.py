62b6c70802453a5245a53ccd3c3e8660ba50ee74
ofry <speitshot@gmail.com>
2022-08-01 19:26:18 +0300

    Fix refuse coach

--- app/domain/user/resources/deleter/postgres.py
+++ app/domain/user/resources/deleter/postgres.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from sqlalchemy import update
+from sqlalchemy import update, delete, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.util._collections import immutabledict
 
@@ -15,6 +15,12 @@ class PostgresUserDeleter(UserDeleter):
 
     async def delete(self, id: UUID):
         query = update(models.User).where(models.User.uuid == id).values(is_deleted=True)
+        await self.session.execute(
+            query,
+            execution_options=immutabledict({'synchronize_session': 'fetch'}),
+        )
+        subquery = select(models.User.id).where(models.User.uuid == id).subquery()
+        query = delete(models.Token).where(models.Token.user_id == subquery)
         await self.session.execute(
             query,
             execution_options=immutabledict({'synchronize_session': 'fetch'}),
